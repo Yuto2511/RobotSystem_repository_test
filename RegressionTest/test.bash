3f28bce96364f110f1ae1e394722a49ef603ce42
Yuto2511 <efmgh2511@outlook.jp>
2022-10-27 12:17:35 +0900

    cleaing up

--- RegressionTest/test.bash
+++ RegressionTest/test.bash
@@ -1,5 +1,27 @@
-#!/bin/bash
+#!/bin/bash -vx
+# SPDX-FileCopyrightTest: 2022 Yamaguchi Yuto
+# SPDX-License-Indentifer: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/0 TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] 	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] 	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
