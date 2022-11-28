de834299b5704dc1f6554d0ca09df930b6a5a276
Yuto2511 <efmgh2511@outlook.jp>
2022-11-28 19:59:00 +0900

    add a test

--- test.bash
+++ test.bash
@@ -10,15 +10,37 @@ ng () {
 res=0
 
 ### I/0 TEST ###
+### plus ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+### four arithmetic operations ###
+out=$(echo "+ 1 2 3" < test_nums | ./four_arithmetic_operations)
+[ "${out}" = 6.0 ] || ng ${LINENO}
+
+out=$(echo "- 1 2 3" < test_nums | ./four_arithmetic_operations)
+[ "${out}" = -6.0 ] || ng ${LINENO}
+
+out=$(echo "* 1 2 3" < test_nums | ./four_arithmetic_operations)
+[ "${out}" = 6.0 ] || ng ${LINENO}
+
+out=$(echo "/ 1 2" < test_nums | ./four_arithmetic_operations)
+[ "${out}" = 1.5 ] || ng ${LINENO}
+
 ### STRANGE INPUT ###
+
+### STRANGE INPUT ###
+### plus ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ] 	  || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
+[ "$?" = 1 ] 	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+### four arithmetic operations ###
+out=$(seq 5 | ./four_arithmetic_operations)
 [ "$?" = 1 ] 	  || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
